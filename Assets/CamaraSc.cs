32d1de1f08a9cbb253577368aac29fa95b58a9ba
mariolopezjuanatey <mariolopezjuanatey@gmail.com>
2022-02-22 12:39:01 +0100

    LVL2 full map, full flags, following camera

---
+++ Assets/CamaraSc.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSc : MonoBehaviour
+{
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Target.transform.position.y - 11, transform.position.z);
+    }
+}
